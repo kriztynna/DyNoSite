9ee8be605395df4aeac9446b8f02dc6aaf510265
Cristina <cristina.colon+git@gmail.com>
2015-07-22 11:28:31 -0400

    project ready

---
+++ app.js
@@ -0,0 +1,8 @@
+var router = require("./router.js");
+
+var http = require('http');
+http.createServer(function (request, response) {
+  router.home(request, response);
+  router.user(request, response);
+}).listen(8000, '127.0.0.1');
+console.log('Server running at http://<workspace-url>/');
